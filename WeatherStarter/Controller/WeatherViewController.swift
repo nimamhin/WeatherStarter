8678ff83bbd5d13b5244fb35ac6381939dd861ff
NIMA M'HIN <nimamhin@NIMAs-MacBook-Pro.local>
2025-06-18 16:52:44 +0100

    Fresh initial commit – WeatherStarter

---
+++ WeatherStarter/Controller/WeatherViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherStarter
+//
+//  Created by NIMA M'HIN on 18/6/2025.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
